4f110a231244456433e7091336c347e40ebdce57
Niharika <98871817+NiharikaKesarwani99@users.noreply.github.com>
2022-03-23 01:38:00 +0530

    added few lines

--- app/src/main/java/com/example/HelloActivity/MainActivity.kt
+++ app/src/main/java/com/example/HelloActivity/MainActivity.kt
@@ -2,16 +2,28 @@ package com.example.HelloActivity
 
 //import android.R
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.example.HelloActivity.databinding.ActivityMainBinding
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttp
+import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
     private var message: TextView? = null
+//    private lateinit var temp: ActivityMainBinding
 
     private var counter = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        temp = ActivityMainBinding.inflate(layoutInflater)
+//        temp.helloImage.setOnClickListener(){
+//             Glide.with(this).load("").into(temp.imageView)
+//        }
+
         setContentView(R.layout.activity_main)
         message = findViewById(R.id.clickCounter)
         val droid = findViewById<ImageView>(R.id.helloImage)
